6fd472ca0d6e384e10b8582005c6e1c947b2305f
SJ <sj@MacBook-Air.local>
2019-05-26 22:33:53 +0900

    Add vehicle api

--- api/vehicle.js
+++ api/vehicle.js
@@ -17,31 +17,32 @@ const router = express.Router();
 router.get('/search', (req, res) => {
   const sql = 'SELECT * FROM VEHICLE';
   connection.query(
-  sql,
-  (err, rows) => {
-    if (err) {
+    sql,
+    (err, rows) => {
+      if (err) {
+        res.send({
+          success: false,
+        });
+        console.log(err);
+        return;
+      }
+
+      // const vehicle_datas = [];
+      // const vehicle_data = {};
+      // Object.keys(row).forEach((key) => {
+      //   const newkey = firstToUpperCase(key);
+      //   vehicle_data[newkey] = row[key];
+      // });
+      // vehicle_datas.push(vehicle_data)
+
+      console.log(rows);
       res.send({
-        success: false,
+        vehicle_data: rows,
+        success: true,
       });
-      console.log(err);
-      return;
-    }
-
-    // const vehicle_datas = [];
-    // const vehicle_data = {};
-    // Object.keys(row).forEach((key) => {
-    //   const newkey = firstToUpperCase(key);
-    //   vehicle_data[newkey] = row[key];
-    // });
-    // vehicle_datas.push(vehicle_data)
-
-    console.log(rows);
-    res.send({
-      vehicle_data: rows,
-      success: true,
-    });
-  },
-)});
+    },
+  )
+});
 
 
 router.post('/join', async (req, res) => {
--- src/components/Vehicles.vue
+++ src/components/Vehicles.vue
@@ -1,3 +1,43 @@
 <template>
-  <div></div>
+  <div id="vehicles">
+    <h1>List of Vehicles</h1>
+    <div v-for="vehicle in vehicles" v-bind:key="vehicle.id">
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center column pa-5>
+          <div class="display-4 font-weight-black white--text text-xs-center">GETS YOU</div>
+          <div class="display-4 font-weight-black white--text text-xs-center mb-3">FROM "A" TO "B"</div>
+          <div
+            class="display-1 font-weight-bold white--text text-xs-center"
+          >A unified transportation rental system</div>
+        </v-layout>
+
+        <img v-bind:src="vehicle.Image">
+      </v-container>
+    </div>
+    <button v-on:click="fetchVehicle()">get data</button>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "vehicles",
+  data: function() {
+    return {
+      vehicles: []
+    };
+  },
+  methods: {
+    fetchVehicle() {
+      this.$Axios
+        .get(`${this.$host}/vehicle/search`)
+        .then(response => {
+          this.vehicles = response.data.vehicle_data;
+          console.log(this.vehicles);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
